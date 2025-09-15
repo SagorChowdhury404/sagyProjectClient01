cc00079821f7547c1e63fefe41f956b88914ad74
sagorchowdhury <rajchowdhury820@gmail.com>
2025-09-15 21:40:40 +0400

    added the peojects card

--- src/pages/ProjectsPage/ProjectAll.jsx
+++ src/pages/ProjectsPage/ProjectAll.jsx
@@ -3,9 +3,12 @@ import Contact from "../shared/contact/Contact";
 import UseProjectApi from "../../hooks/UseProjectApi";
 import ProjectCard from "../HomeProjects/projectCard/ProjectCard";
 import LoadingPage from "../shared/loadingPage/LoadingPage";
+import { useState } from "react";
 
 const ProjectAll = () => {
     const [projects, loading] = UseProjectApi();
+    const [activeTab, setActiveTab] = useState("Web");
+
     if (loading) return <LoadingPage></LoadingPage >;
 
     //webApp Landing Static 
@@ -15,98 +18,158 @@ const ProjectAll = () => {
     const Landing = projects.filter(project => project.projectCategory === "Landing");
     const Static = projects.filter(project => project.projectCategory === "Static");
 
-
     return (
 
         <div>
             <Helmet> <title>ProjectAll</title> </Helmet>
 
+
+
             <section>
-                <div className="flex items-center -mx-4 overflow-x-auto overflow-y-hidden sm:justify-center flex-nowrap dark:bg-gray-100 dark:text-gray-800 my-5 ">
-                    <a rel="noopener noreferrer" href="#" className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border-b dark:border-gray-600 dark:text-gray-600">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
-                            <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
-                        </svg>
-                        <span> Web </span>
+                <section className="flex items-center -mx-4 overflow-x-auto overflow-y-hidden sm:justify-center flex-nowrap dark:bg-gray-100 dark:text-gray-800 my-5">
+
+
+                    {/* Web */}
+                    <a
+                        onClick={() => setActiveTab("Web")}
+                        className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 cursor-pointer 
+            ${activeTab === "Web"
+                                ? "border border-b-0 rounded-t-lg dark:border-gray-600 dark:text-blue-600 "
+                                : "border-b dark:border-gray-600 dark:text-gray-600"}`}
+                    >
+                        <span>Web</span>
                     </a>
-                    <a rel="noopener noreferrer" href="#" className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border border-b-0 rounded-t-lg dark:border-gray-600 dark:text-gray-900">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
-                            <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
-                            <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
-                        </svg>
-                        <span>Seo </span>
+
+
+
+
+
+
+
+
+
+                    {/* SEO */}
+                    <a
+                        onClick={() => setActiveTab("SEO")}
+                        className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 cursor-pointer 
+            ${activeTab === "SEO"
+                                ? "border border-b-0 rounded-t-lg dark:border-gray-600 dark:text-blue-600"
+                                : "border-b dark:border-gray-600 dark:text-gray-600"}`}
+                    >
+                        <span>SEO </span>
                     </a>
-                    <a rel="noopener noreferrer" href="#" className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border-b dark:border-gray-600 dark:text-gray-600">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
-                            <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
-                        </svg>
+
+                    {/* LinkedIn */}
+                    <a
+                        onClick={() => setActiveTab("LinkedIn")}
+                        className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 cursor-pointer 
+            ${activeTab === "LinkedIn"
+                                ? "border border-b-0 rounded-t-lg dark:border-gray-600 dark:text-blue-600"
+                                : "border-b dark:border-gray-600 dark:text-gray-600"}`}
+                    >
                         <span>LinkedIn</span>
                     </a>
-
-                </div>
-            </section>
-
-
-
-            <h1 className='before:block before:w- before:h-3 before:mb-5 before:rounded-md before:mx-auto before:dark:bg-violet-600 '></h1>
-
-
-
-
-            {/* Web App / Dynamic Website */}
-            <div className="text-center mb-14 ">
-                <h3 className="text-3xl font-semibold"> Web App / Dynamic Website</h3>
-
-                <h1 className='before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto before:dark:bg-violet-600'> </h1>
-
-
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-8 px-4 md:px-16 py-10">
-                    {webApp.map(project => <ProjectCard project={project} ></ProjectCard>
-
-
-                    )}
-                </div>
-            </div>
-
-            {/* Landing Page Website */}
-            <div className="text-center mb-14 ">
-                <h3 className="text-3xl font-semibold">Landing Page Website</h3>
-
-                <h1 className='before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto before:dark:bg-violet-600'> </h1>
-
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-8 px-4 md:px-16 py-10">
-                    {Landing.map(project => <ProjectCard project={project} ></ProjectCard>
-
-
-                    )}
-                </div>
-
-                <section className="card">
-
                 </section>
 
 
-            </div>
 
-            {/* //Static Website */}
-            <div className="text-center mb-14 ">
-                <h3 className="text-3xl font-semibold">Static Website </h3>
-                {/* <span className="text-sm font-bold tracking-wider uppercase dark:text-gray-600">Technologies I Work With</span> */}
-                <h1 className='before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto before:dark:bg-violet-600'> </h1>
+                {/* //////////////////////////////////////////////// */}
+
+                <h1 className='before:block before:w- before:h-3 before:mb-5 before:rounded-md before:mx-auto before:dark:bg-violet-600 '></h1>
 
 
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-8 px-4 md:px-16 py-10">
-                    {Static.map(project => <ProjectCard project={project} ></ProjectCard>
+                {/* Show selected tab content */}
 
 
-                    )}
+
+                {/* Web App / Dynamic Website */}
+                <div className="p-4">
+                    {activeTab === "Web" &&
+
+                        <section>
+
+                            {/* //Web App */}
+                            <div className="text-center mb-14 ">
+                                <h3 className="text-3xl font-semibold"> Web App / Dynamic Website</h3>
+
+                                <h1 className='before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto before:dark:bg-violet-600'> </h1>
+
+
+                                <div className="grid grid-cols-1 md:grid-cols-3 gap-8 px-4 md:px-16 py-10">
+                                    {webApp.map(project => <ProjectCard project={project} ></ProjectCard>
+
+
+                                    )}
+                                </div>
+                            </div>
+
+
+                            {/* //Landing Website */}
+                            <div className="text-center mb-14 ">
+                                <h3 className="text-3xl font-semibold">Static Website </h3>
+                                {/* <span className="text-sm font-bold tracking-wider uppercase dark:text-gray-600">Technologies I Work With</span> */}
+                                <h1 className='before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto before:dark:bg-violet-600'> </h1>
+
+
+                                <div className="grid grid-cols-1 md:grid-cols-3 gap-8 px-4 md:px-16 py-10">
+                                    {Landing.map(project => <ProjectCard project={project} ></ProjectCard>
+
+
+                                    )}
+                                </div>
+
+                            </div>
+                            {/* //Static Website */}
+                            <div className="text-center mb-14 ">
+                                <h3 className="text-3xl font-semibold">Static Website </h3>
+                                {/* <span className="text-sm font-bold tracking-wider uppercase dark:text-gray-600">Technologies I Work With</span> */}
+                                <h1 className='before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto before:dark:bg-violet-600'> </h1>
+
+
+                                <div className="grid grid-cols-1 md:grid-cols-3 gap-8 px-4 md:px-16 py-10">
+                                    {Static.map(project => <ProjectCard project={project} ></ProjectCard>
+
+
+                                    )}
+                                </div>
+
+                            </div>
+
+                        </section>
+
+
+
+                    }
+
+
+
+
+
+                    {activeTab === "SEO" && <section> <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nulla in voluptatibus eligendi porro excepturi eveniet ipsam architecto non vel labore, beatae dolores quae sed veniam. Cumque voluptate tempore consectetur sit maxime, in, beatae unde qui non natus sed iusto voluptas nam! Eaque numquam illum maiores beatae provident voluptatem quasi, doloribus facere ratione porro impedit hic sed nesciunt soluta, laborum recusandae voluptatibus similique quae, dolores voluptatum. Commodi quod dolorum temporibus asperiores harum, aliquam sunt praesentium vitae eaque autem repudiandae aut! Impedit assumenda laboriosam numquam, quae blanditiis eveniet reprehenderit beatae consectetur nulla possimus saepe expedita amet itaque optio rerum sapiente tempora!</h1></section>
+                    }
+
+
+
+
+
+                    {activeTab === "LinkedIn" && <section> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis, placeat aut. Placeat qui distinctio doloremque dolores fugiat accusantium officiis illum!</section>}
                 </div>
+            </section >
 
-            </div>
+
+
+
+
+
+
+
+
+
+
 
 
             <Contact></Contact>
-        </div>
+        </div >
     );
 };
 
